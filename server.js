e740da75f59c7cfd6adfba67ca45f796cac765a5
Sung Kim <sungjk77@msn.com>
2021-05-14 15:02:42 -0700

    deploying

--- server.js
+++ server.js
@@ -16,7 +16,13 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", 
+{
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+});
 
 app.get(`/stats`, (req, res) => res.sendFile(path.join(__dirname, `public/stats.html`)));
 app.get(`/exercise`, (req, res) => res.sendFile(path.join(__dirname, `public/exercise.html`)));
